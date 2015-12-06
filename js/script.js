ff182ecb33a916cb54300c54566dba92c0ae83ba
Arminas <arminasb7@gmail.com>
2015-12-06 09:03:36 +0000

    Prevent to show two quotes in a row

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	generate();
+	//generator function
+	function generate(){
 	var quotes = ["Some days you just have to create your own sunshine.",
 			      "Be somebody who makes everybody feel lika a somebody.",
 			      "Never give up! There is no such thing as an ending jus a new beginning.",
@@ -9,11 +12,18 @@ $(document).ready(function(){
 			      "I'm not who I was before!",
 			      "Being happy doesn't mean everything's perfect. It means you have decided to look beyond the impperfections.",
 			      "Work hard, dreambig."]
-	$(".button").on("click", function(){
+	//prevent to show same quote twice
+	var randno = 0;
+	do {
 	var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+	} while (randomQuote == randno)
+	randno = randomQuote;
 	$(".itiem").text(randomQuote);
-});
-
+	}
+	//button click function
+	$(".button").on("click", function(){
+	 	generate();
+	});
 });
 
 
